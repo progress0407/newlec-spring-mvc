9bce8d46a51661144fa7fa1e6648abcb11c236f6
swcho <progress0407@naver.com>
2021-07-22 23:00:03 +0900

    체크 셀렉트박스 추가

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -2,6 +2,7 @@ package com.newlecture.web.controller.admin.board;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("adminNoticeController")
@@ -16,7 +17,14 @@ public class NoticeController {
     
     @RequestMapping("reg")
     @ResponseBody
-    public String reg(String title, String content, String category) {
+    public String reg(String title, String content, String category, @RequestParam("back-exercises") String[] backExercises, 
+	    @RequestParam("rd-back-exercise") String rdBackExercise) {
+	
+	System.out.println("rdBackExercise : " + rdBackExercise);
+	
+	for (String backExercise : backExercises) {
+	    System.out.println(backExercise);
+	}
 	
 	return String.format("title : %s <br>  content : %s <br> category : %s", title, content, category);
     }
